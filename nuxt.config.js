638817d8a9651b49591744748f2bc4a37c479d2e
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-05 14:59:58 +0900

    feat: set title, description

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,15 @@
 export default {
   mode: 'universal',
   srcDir: 'src',
-  /*
-   ** Headers of the page
-   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Amon Sawamura',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'さわむらあもんのページ',
       },
     ],
     link: [
