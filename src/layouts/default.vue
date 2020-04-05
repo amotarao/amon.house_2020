28fbc9fc5014c81b52f5e51091137702b087aa83
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-05 15:52:58 +0900

    feat: add canonical link

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,7 +5,15 @@
 <script lang="ts">
 import Vue from 'vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  head() {
+    const canonicalLink = `https://amotarao.dev/${this.$route.path}/`.replace(/\/+$/, '/');
+
+    return {
+      link: [{ hid: 'canonical', rel: 'canonical', href: canonicalLink }],
+    };
+  },
+});
 </script>
 
 <style>
