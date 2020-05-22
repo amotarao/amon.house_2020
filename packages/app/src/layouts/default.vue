ce2117efbf7f57a55f721776edbb5c4c9f75637a
Amon Sawamura <amon.sawamura@gmail.com>
2020-05-22 22:32:43 +0900

    feat(home): add to-home button

--- packages/app/src/layouts/default.vue
+++ packages/app/src/layouts/default.vue
@@ -1,5 +1,13 @@
 <template>
-  <nuxt />
+  <div :class="$style.container">
+    <nuxt-link v-if="$route.path !== '/'" to="/" :class="$style.home">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 24 24" aria-label="ホームにもどる">
+        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+        <path d="M0 0h24v24H0z" fill="none" />
+      </svg>
+    </nuxt-link>
+    <nuxt />
+  </div>
 </template>
 
 <script lang="ts">
@@ -19,3 +27,28 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style lang="scss" module>
+.container {
+  height: 100%;
+}
+
+.home {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: $bg2;
+  border-radius: 4px;
+  color: $text;
+  position: sticky;
+  top: 16px;
+  margin-left: 16px;
+  margin-bottom: -48px;
+  height: 48px;
+  width: 48px;
+
+  svg {
+    fill: currentColor;
+  }
+}
+</style>
